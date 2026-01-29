82832d2e7be7644288a6e6cc821819fb45ff3ee6
TEHNAN <93577025+TEHNAN@users.noreply.github.com>
2026-01-29 08:54:00 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,80 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 17:32:56 2026
+
+@author: BBarsch
+"""
+
 import streamlit as st
-import pipeline_functions as pf
 
-st.set_page_config(page_title="Top Apps Pipeline", layout="wide")
-st.title("📊 Top Apps Pipeline (Dynamic Filters)")
-st.write("Select the filters below, then run the pipeline and view the result.")
+st.write("Hello2")
 
-# ---- Category options ----
-CATEGORIES = [
-    "ART_AND_DESIGN",
-    "AUTO_AND_VEHICLES",
-    "BEAUTY",
-    "BOOKS_AND_REFERENCE",
-    "BUSINESS",
-    "COMICS",
-    "COMMUNICATION",
-    "DATING",
-    "EDUCATION",
-    "ENTERTAINMENT",
-    "EVENTS",
-    "FAMILY",
-    "FINANCE",
-    "FOOD_AND_DRINK",
-    "GAME",
-]
+st.title("Title heading")
 
-# ---- Controls (main page) ----
-col1, col2, col3 = st.columns(3)
+st.write("Hello, Streamlit!")
 
-with col1:
-    category = st.selectbox(
-        "Category",
-        options=CATEGORIES,
-        index=CATEGORIES.index("FOOD_AND_DRINK"),
-    )
+st.header("Number selection")
 
-with col2:
-    min_rating = st.number_input(
-        "Minimum rating",
-        min_value=0.0,
-        max_value=5.0,
-        value=4.0,
-        step=0.1,
-    )
-
-with col3:
-    min_reviews = st.number_input(
-        "Minimum reviews",
-        min_value=0,
-        value=1000,
-        step=100,
-    )
-
-st.divider()
-
-# ---- Run pipeline ----
-if st.button("▶ Run pipeline"):
-    with st.spinner("Running pipeline..."):
-        apps_data = pf.extract("apps_data.csv")
-        reviews_data = pf.extract("review_data.csv")
-
-        top_apps_data = pf.transform(
-            apps=apps_data,
-            reviews=reviews_data,
-            category=category,
-            min_rating=float(min_rating),
-            min_reviews=int(min_reviews),
-        )
-
-        pf.load(
-            dataframe=top_apps_data,
-            database_name="market_research",
-            table_name="top_apps"
-        )
-
-    st.success("Pipeline completed!")
-    st.write(f"Returned rows: {len(top_apps_data)}")
-    st.dataframe(top_apps_data, use_container_width=True)
-
-
+number = st.slider("Pick a number", 1, 100)
+st.write(f"You picked: {number}")
