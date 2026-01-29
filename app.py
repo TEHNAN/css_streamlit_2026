0bd10a606b927848c588925a4d6dea007dbff56b
TEHNAN <93577025+TEHNAN@users.noreply.github.com>
2026-01-29 08:29:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pipeline_functions as pf
+#import pipeline_functions as pf
 
 st.set_page_config(page_title="Top Apps Pipeline", layout="wide")
 st.title("📊 Top Apps Pipeline (Dynamic Filters)")
@@ -76,3 +76,4 @@ if st.button("▶ Run pipeline"):
     st.success("Pipeline completed!")
     st.write(f"Returned rows: {len(top_apps_data)}")
     st.dataframe(top_apps_data, use_container_width=True)
+
